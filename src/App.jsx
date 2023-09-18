56dab4068602c72701bbaeded1143a31cff3fc00
SarkerFardinFeroz <sarkerfardinferoz1@gmail.com>
2023-09-18 08:54:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <DaisyNav />
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
